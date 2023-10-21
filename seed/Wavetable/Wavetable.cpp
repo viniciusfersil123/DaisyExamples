d1b5f4e2817e4b69ca70bdb7dd90be2aaea6e03e
viniciusfersil123 <viniciusfersil@gmail.com>
2023-10-21 18:25:15 -0300

    wip: just phasor2 working

--- seed/Wavetable/Wavetable.cpp
+++ seed/Wavetable/Wavetable.cpp
@@ -11,6 +11,7 @@ using namespace daisy::seed;
 
 DaisySeed hw;
 Phasor    phs;
+Phasor    phs2;
 float     PI               = 3.14159265358979323846f;
 const int sampleSize       = 10463;
 float     cosEnv[256]      = {0};
@@ -19,7 +20,12 @@ float     potValue         = 1;
 float     potValue2        = 1;
 float     potValue3        = 1;
 float     speed            = 100;
+bool      state            = true;
 const int num_adc_channels = 3;
+float     ms               = 0;
+float     samps            = 0;
+float     transposition    = 0;
+float     transp           = 0;
 
 uint32_t wrapIdx(uint32_t idx, uint32_t size)
 {
@@ -32,15 +38,26 @@ uint32_t wrapIdx(uint32_t idx, uint32_t size)
     return idx;
 }
 
+float ms2samps(float ms, float samplerate)
+{
+    return (ms * 0.001f) * samplerate;
+}
+
+float cents2ratio(float cents)
+{
+    return pow(2, cents / 1200);
+}
+
 void AudioCallback(AudioHandle::InputBuffer  in,
                    AudioHandle::OutputBuffer out,
                    size_t                    size)
 {
     for(size_t i = 0; i < size; i++)
     {
-        uint32_t idx
-            = wrapIdx((uint32_t)(phs.Process() * sampleSize), sampleSize);
-        float sig = sample[idx] * cosEnv[(uint32_t)(phs.Process() * 256)];
+        uint32_t idxTransp = (uint32_t)(phs.Process() * samps);
+        uint32_t idxSpd    = (uint32_t)(phs2.Process() * sampleSize);
+        float    sig = sample[(uint32_t)(wrapIdx((idxSpd + idxTransp), sampleSize))]
+                    * cosEnv[(uint32_t)(phs2.Process() * 256)];
         out[0][i] = out[1][i] = sig;
     }
 }
@@ -51,7 +68,8 @@ int main(void)
     hw.Init();
     float sample_rate = hw.AudioSampleRate();
     phs.Init(sample_rate);
-    phs.SetFreq((sample_rate * potValue / sampleSize));
+    phs2.Init(sample_rate);
+    phs2.SetFreq((sample_rate * potValue / sampleSize));
     AdcChannelConfig my_adc_config[num_adc_channels];
     my_adc_config[0].InitSingle(hw.GetPin(15));
     my_adc_config[1].InitSingle(hw.GetPin(16));
@@ -71,9 +89,14 @@ int main(void)
         potValue  = hw.adc.GetFloat(0);
         potValue2 = hw.adc.GetFloat(1);
         potValue3 = hw.adc.GetFloat(2);
-        //chunkSize = potValue2 * sampleSize;
-        //maps the pot value to the frequency of the phasor exponentially
-        speed = ((pow(2, potValue * 10)) / 100) * (sample_rate/sampleSize);
-        phs.SetFreq(speed);
+        ms        = pow(2, potValue * 2400);
+        // maps 0-1 potValue to -1200 to 1200 cents
+        transposition = cents2ratio((potValue2 * 2400) - 1200);
+        samps         = ms2samps(ms, sample_rate);
+        speed = ((pow(2, potValue * 10)) / 100) * (sample_rate / sampleSize);
+        transp
+            = (transposition - ((pow(2, potValue * 10)) / 100)) * (1000 / ms);
+        phs.SetFreq(transp);
+        phs2.SetFreq(speed);
     }
 }
